f5f74cc46fcef3abe72de67cfe5350c30c9217c9
TAL GABBAY <TALGAB@rafael.co.il>
2022-08-01 09:36:08 +0300

    adding init files, directions and source files

---
+++ main.py
@@ -0,0 +1,40 @@
+from src.logic_layer import api_calls
+import json
+import re
+
+api_calls.add_user('Tal Gabbay', 'tg')
+r = api_calls.get_user('Tal Gabbay')
+
+# api_calls.add_friend('Tal Gabbay', 'tg', 'Adam')
+# api_calls.add_playlist('Tal Gabbay', 'tg', 'summer vibes')
+# api_calls.add_song("Regge", '1970', "Bob Marly", "One Love")
+# api_calls.get_song("One Love")
+# api_calls.song_upvote('Tal Gabbay', 'tg', 'summer vibes', 'One Love')
+# api_calls.song_downvote('Tal Gabbay', 'tg', 'summer vibes', 'One Love')
+# api_calls.get_song("One Love")
+# api_calls.add_song_to_playlist('Tal Gabbay', 'tg', 'summer vibes', 'One Love')
+# api_calls.get_playlist('Tal Gabbay', 'tg', 'summer vibes')
+# api_calls.get_song_by_rank('0', 'eq')
+# api_calls.change_user_password('Tal Gabbay', '0', 'tg')
+# api_calls.delete_all_songs()
+# r1 = api_calls.delete_all_users()
+# api_calls.set_songs_into_the_db()
+# api_calls.set_users_into_the_db()
+
+
+password = "Tal"
+b = r.text
+m = re.search(password, b)
+print(m.group(0))
+
+
+playlist = "gababaggds"
+def t_add_playlist(playlist):
+    add_user_response = api_calls.add_user("User1", "User1")
+    get_user_response = api_calls.get_user("User1")
+    add_playlist_response = api_calls.add_playlist("User1", "User1", playlist)
+    json_format = json.loads(get_user_response.text)
+    print(json_format["data"]["playlists"])
+
+
+t_add_playlist(playlist)
